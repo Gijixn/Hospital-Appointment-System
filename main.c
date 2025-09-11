47d5c8a562ef70074e8d483f7da18e5b5e4dcc50
Gijixn <139514776+Gijixn@users.noreply.github.com>
2025-09-11 06:20:59 +0000

    new thing

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    typedef struct
+    {
+        int Pat_ID;
+        int Date;
+        char Pat_Name[50];
+    } Patient;
+
+    Patient users[100];
+     int RandomID = 1000 + rand() % 900;
+
+     0
+
+    
+    
+
+    printf("Enter your name: ")
+
+}
