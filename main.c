0c607392276b9ef0f90d37fa18f1df599bb39187
Gijixn <139514776+Gijixn@users.noreply.github.com>
2025-09-17 00:55:50 +0000

    Missing stuff

--- main.c
+++ main.c
@@ -1,23 +1,79 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main()
 {
+    FILE* dataptr;
+
+    dataptr = fopen("data.txt", "r+");
+
+    if (dataptr == NULL)
+    {
+        printf("File does not exist");
+        return 0;
+    }
+
     typedef struct
     {
         int Pat_ID;
         int Date;
         char Pat_Name[50];
-    } Patient;
+    } User;
 
-    Patient users[100];
-     int RandomID = 1000 + rand() % 900;
+    User inpatient[100];
+    User outpatient[100];
+    char test[50];
+    int inpatient_Registered = 0;
+    int outpatient_Registered = 0;
+    
+    int unique_IDGen()
+    {
+        // store and check for existing id
+        // generate 1000000 + rand() % 9000001;
+    }
+    int menu()
+    {
+        int choice;
+        printf("\n1. Inpatient Registration\n");
+        printf("2. Outpatient Registration\n");
+        printf("3. Exit\n\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        return choice;
+    }
 
-     0
+    int patient_Registration(int patient_Type)
+    {
+        switch(patient_Type)
+        {
+            case 1:
+            printf("Enter your name: ");
+            scanf("%s", test);
+            fputs(test, dataptr);
+            fputs("\n", dataptr);
+            break;
+            case 2:
+            //outpatient store
+            break;
+            default:
+            printf("Invalid Input in patient_Registration");
+            break;
+        }
+    }
+
+    while(1)
+    {
+        int choice = menu();
+        if (choice == 3)
+        {
+            break;
+        }
+        patient_Registration(choice);
+    }
 
     
-    
-
-    printf("Enter your name: ")
+    fclose(dataptr);
+        printf("The file is now closed.\n");
 
 }
