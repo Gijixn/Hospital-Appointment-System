d56a2fe23baf775a4e748517d251d4102000d3f5
Gijixn <139514776+Gijixn@users.noreply.github.com>
2025-10-26 16:58:38 +0000

    new stuff

--- main.c
+++ main.c
@@ -1,32 +1,17 @@
-#include <stdio.h>
+                                 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
-int main()
-{
-    FILE* data; //opening file - if file doesn't exist, it will create a new one
-    data = fopen("patientRecords.bin", "r+");
-    if (data == NULL)
+typedef struct
     {
-        fopen("patientRecords.bin", "w+");
-        if (data == NULL)
-        {
-            printf("ERROR : File cannot be accessed\n");
-            return 1;
-        }
-    }
-
-    typedef struct
-    {
-        int ordinal_ID;
         int Pat_ID;
         int date;
-        char Pat_Type;
         char Pat_Name[50];
     } User;
-    User Patient;
     
-    int menu()
+int menu()
     {
         int choice;
         printf("\n1. Inpatient Registration\n");
@@ -37,55 +22,72 @@ int main()
         return choice;
     }
 
-    int last_ID(data)
-    {
-        User buffer;
-        if (ftell(data) == 0)
-        {
-            printf("No content within the file.\n");
-            return 1;
-        } else
-        {
-            fseek(data, -sizeof(User), SEEK_END);
-            fread(buffer, sizeof(User), 1, data);
-            return buffer.ordinal_ID;
-
-        }
-    }
-
-    int patient_Order = last_ID(data);
-
-        int unique_IDGen()
-    {
-        // loop through all unique ids while (fread(   ))
-    }
-
+int unique_IDGen()
+{
+    // loop through all unique ids while (fread(   ))
     
-    int patient_Registration(int patient_Type)
+}
+
+
+// int patient_Registration(int patient_Type)
+// {
+//     if (patient_Type == 1)
+//     {
+//         printf("What is your name?: ");
+//         scanf("%[^\n]", Patient.name);
+
+
+        
+//     }
+// }
+
+bool patWriteData(User data, int choice)
+{
+    if (choice == 1)
     {
-        if (patient_Type == 1)
-        {
-            printf("What is your name?: ");
-            scanf("%[^\n]", Patient.name);
+        FILE *outData;
+        outData = fopen("outpatientRecords.bin", "ab");
+        
+        if (outData == NULL) return false;
 
+        if (fwrite(&data, sizeof(User), 1, outData) != 1) return false;
 
-            
-        }
+        if (fclose(outData)  == EOF) return false;
+
     }
 
-
-    while(1)
+       else if (choice == 2)
     {
-        int choice = menu();
-        if (choice == 3)
-        {
-            break;
-        }
-        patient_Registration(choice);
-    }
+        FILE *inData;
+        inData = fopen("inpatientRecords.bin", "ab");
+        
+        if (inData == NULL) return false;
 
-    
-    fclose(data);
-        printf("The file is now closed.\n");
+        if (fwrite(&data, sizeof(User), 1, inData) != 1) return false;
 
+        if (fclose(inData)  == EOF) return false;
+
+    } else return false;
+
+}
+
+int main()
+{
+    User buffer;
+
+    buffer.Pat_ID = 20;
+    buffer.date = 12;
+    strcpy(buffer.Pat_Name, "David");
+
+    patWriteData(buffer, 1);
+
+    // while(1)
+    // {
+    //     int choice = menu();
+    //     if (choice == 3)
+    //     {
+    //         break;
+    //     }
+    //     patient_Registration(choice);
+    // }
 }
